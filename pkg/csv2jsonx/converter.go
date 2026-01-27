1967ac065422cc16a5098f5d070055ad3f5d499c
Lavisha Gupta <lavisha.gupta@siemens.com>
2026-01-27 22:43:54 +0530

    feat: add public CSV to JSON API

---
+++ pkg/csv2jsonx/converter.go
@@ -0,0 +1,11 @@
+package csv2jsonx
+
+import "io"
+
+func ConvertReader(r io.Reader) ([]byte, error) {
+	return nil, nil
+}
+
+func ConvertFile(filePath string) ([]byte, error) {
+	return nil, nil
+}
